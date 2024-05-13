3810815f4f0ce12c1c788c7ed419940de6592b8e
Luis Zamorano <lzamorano20@alumnos.utalca.cl>
2024-05-12 23:32:51 -0400

    Mostrando alertas

--- src/app/pages/alertas/alertas.component.ts
+++ src/app/pages/alertas/alertas.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FirebaseService} from "../../services/firebase.service";
+import {Mensaje} from "../../interfaces/Mensaje";
 
 @Component({
   selector: 'app-alertas',
@@ -8,5 +10,18 @@ import { Component } from '@angular/core';
   styleUrl: './alertas.component.css'
 })
 export class AlertasComponent {
+  protected firebaseService = inject(FirebaseService)
+  messages: Mensaje[] = [];
 
+  constructor() { }
+
+  ngOnInit(): void {
+    // Llama al método para obtener mensajes en tiempo real desde Firebase Realtime Database
+    this.firebaseService.obtenerMensajesEnTiempoReal();
+
+    // Escucha los cambios en los mensajes y actualiza la lista de mensajes
+    this.firebaseService.messagesUpdated.subscribe((messages: Mensaje[]) => {
+      this.messages = messages;
+    });
+  }
 }
--- src/app/pages/lista-delincuentes/lista-delincuentes.component.ts
+++ src/app/pages/lista-delincuentes/lista-delincuentes.component.ts
@@ -1,12 +1,53 @@
 import { Component } from '@angular/core';
+import {FirebaseService} from "../../services/firebase.service";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-lista-delincuentes',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './lista-delincuentes.component.html',
   styleUrl: './lista-delincuentes.component.css'
 })
 export class ListaDelincuentesComponent {
+  carpetas: { nombre: string, images: any[] }[] = [];
+  nuevaCarpetaNombre: string = '';
+
+  constructor(private firebaseService: FirebaseService) {}
+
+  ngOnInit(): void {
+    this.listarCarpetas();
+
+    this.firebaseService.imagesUpdated.subscribe(() => {
+      this.carpetas = []
+      this.listarCarpetas();
+    });
+  }
+
+  async listarCarpetas() {
+    const nombresCarpetas = await this.firebaseService.listarCarpetas();
+    for (const carpeta of nombresCarpetas) {
+      const imagenes = await this.firebaseService.listImagesInFolder(carpeta);
+      this.carpetas.push({ nombre: carpeta, images: imagenes });
+    }
+  }
+
+  onFileSelected(event: any, carpeta: string) {
+    const file: File = event.target.files[0];
+    if (file) {
+      this.firebaseService.subirImagen(carpeta, file);
+    }
+  }
+
+  async agregarCarpeta() {
+    if (this.nuevaCarpetaNombre.trim() !== '') {
+      await this.firebaseService.crearCarpeta(this.nuevaCarpetaNombre);
+      this.nuevaCarpetaNombre = ''; // Limpiar el input después de agregar la carpeta
+    } else {
+      console.log('Ingrese un nombre para la carpeta');
+    }
+  }
 
 }
