3acc43233bf4c5bc26493d9111016d6aed0163b6
Luis Zamorano <lzamorano20@alumnos.utalca.cl>
2024-05-12 21:23:10 -0400

    feat(frontend): configuración inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {ListaDelincuentesComponent} from "./pages/lista-delincuentes/lista-delincuentes.component";
+import {AlertasComponent} from "./pages/alertas/alertas.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "", component: ListaDelincuentesComponent},
+  {path: "alertas", component: AlertasComponent},
+  {path: "**", component: ListaDelincuentesComponent}
+];
---
+++ src/app/pages/lista-delincuentes/lista-delincuentes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-delincuentes',
+  standalone: true,
+  imports: [],
+  templateUrl: './lista-delincuentes.component.html',
+  styleUrl: './lista-delincuentes.component.css'
+})
+export class ListaDelincuentesComponent {
+
+}
